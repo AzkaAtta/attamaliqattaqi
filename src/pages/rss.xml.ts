9d83f484dd1805e64696f14ae78f5e6d72d67dc2
auradigital.id <143815833+AzkaAtta@users.noreply.github.com>
2025-08-02 22:55:58 +0700

    Update rss.xml.ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,11 +1,13 @@
 import { getRssString } from '@astrojs/rss';
-
-import { SITE, METADATA, APP_BLOG } from 'astrowind:config';
+import config from '~/config.yaml';
 import { fetchPosts } from '~/utils/blog';
 import { getPermalink } from '~/utils/permalinks';
 
+const { site: SITE, metadata: METADATA, apps } = config;
+const APP_BLOG = apps.blog;
+
 export const GET = async () => {
-  if (!APP_BLOG.isEnabled) {
+  if (!APP_BLOG?.isEnabled) {
     return new Response(null, {
       status: 404,
       statusText: 'Not found',
@@ -17,15 +19,13 @@ export const GET = async () => {
   const rss = await getRssString({
     title: `${SITE.name}’s Blog`,
     description: METADATA?.description || '',
-    site: import.meta.env.SITE,
-
+    site: SITE.site, // ganti ini dari `import.meta.env.SITE`
     items: posts.map((post) => ({
       link: getPermalink(post.permalink, 'post'),
       title: post.title,
       description: post.excerpt,
       pubDate: post.publishDate,
     })),
-
     trailingSlash: SITE.trailingSlash,
   });
 
