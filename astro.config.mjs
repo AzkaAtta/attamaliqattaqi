5db4b92e3a86f9e8f78107fb69db17ac13bb6ecd
auradigital.id <143815833+AzkaAtta@users.noreply.github.com>
2025-08-02 22:29:41 +0700

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://auradigital.id', // ← Ganti dengan domain kamu
+  integrations: [sitemap()],
+});
