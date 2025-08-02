ffc100d60d15a104b693577cd719400cafe740bf
auradigital.id <143815833+AzkaAtta@users.noreply.github.com>
2025-08-03 01:39:50 +0700

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,39 +1,24 @@
-// src/env.d.ts
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../.astro/types.d.ts" />
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+<<<<<<< HEAD
+// Versi dari local Anda (HEAD)
 /// <reference types="astro/client" />
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="vite/client" />
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+interface MyYamlData {
+  SITE: {
+    name: string;
+  };
+}
+=======
+// Versi dari remote (origin/main)
+/// <reference types="astro/client" />
+/// <reference types="vite/client" />
 /// <reference types="../vendor/integration/types" />
-
 declare module '*.yaml' {
   interface MyYamlData {
     SITE: {
       name: string;
       website: string;
       trailingSlash: boolean;
-      title?: string;
-      description?: string;
-    };
-    METADATA: {
-      description: string;
-      title?: string;
-    };
-    APP_BLOG?: {
-      isEnabled?: boolean;
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      list?: any; // Tambahkan komentar ini jika Anda tidak ingin mempersempit tipe 'list'
-    };
-    site?: {
-      name?: string;
-      description?: string;
-    };
-    contact?: {
-      email?: string;
     };
   }
-  const data: MyYamlData;
-  export default data;
 }
+>>>>>>> <commit_hash_remote>
