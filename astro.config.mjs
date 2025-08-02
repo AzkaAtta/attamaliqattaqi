2c79691776fe76fb1f8c04b1f9e2f3b377602f49
auradigital.id <143815833+AzkaAtta@users.noreply.github.com>
2025-08-02 22:31:56 +0700

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-  site: 'https://auradigital.id', // ← Ganti dengan domain kamu
-  integrations: [sitemap()],
-});
