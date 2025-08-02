2678a6f8d7729a076740a59637976db4776b9e41
auradigital.id <143815833+AzkaAtta@users.noreply.github.com>
2025-08-02 22:58:09 +0700

    Update rss.xml.ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,37 +1,38 @@
 import { getRssString } from '@astrojs/rss';
-import config from '~/config.yaml';
-import { fetchPosts } from '~/utils/blog';
-import { getPermalink } from '~/utils/permalinks';
 
-const { site: SITE, metadata: METADATA, apps } = config;
-const APP_BLOG = apps.blog;
+import { SITE, METADATA, APP_BLOG } from 'astrowind:config';
+import { fetchPosts } from '/utils/permalinks';
 
 export const GET = async () => {
-  if (!APP_BLOG?.isEnabled) {
-    return new Response(null, {
-      status: 404,
-      statusText: 'Not found',
-    });
-  }
+if (!APP_BLOG.isEnabled) {
+return new Response(null, {
+status: 404,
+statusText: 'Not found',
+});
+}
 
-  const posts = await fetchPosts();
+const posts = await fetchPosts();
 
-  const rss = await getRssString({
-    title: `${SITE.name}’s Blog`,
-    description: METADATA?.description || '',
-    site: SITE.site, // ganti ini dari `import.meta.env.SITE`
-    items: posts.map((post) => ({
-      link: getPermalink(post.permalink, 'post'),
-      title: post.title,
-      description: post.excerpt,
-      pubDate: post.publishDate,
-    })),
-    trailingSlash: SITE.trailingSlash,
-  });
+const rss = await getRssString({
+title: ${SITE.name}’s Blog,
+description: METADATA?.description || '',
+site: import.meta.env.SITE,
 
-  return new Response(rss, {
-    headers: {
-      'Content-Type': 'application/xml',
-    },
-  });
+items: posts.map((post) => ({  
+  link: getPermalink(post.permalink, 'post'),  
+  title: post.title,  
+  description: post.excerpt,  
+  pubDate: post.publishDate,  
+})),  
+
+trailingSlash: SITE.trailingSlash,
+
+});
+
+return new Response(rss, {
+headers: {
+'Content-Type': 'application/xml',
+},
+});
 };
+
