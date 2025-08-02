59413358964f7532b9714d78867074da7fba2ebf
auradigital.id <143815833+AzkaAtta@users.noreply.github.com>
2025-08-03 01:25:00 +0700

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,39 @@
+// src/env.d.ts
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="astro/client" />
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="vite/client" />
-/// <reference types="../vendor/integration/types.d.ts" />
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="../vendor/integration/types" />
+
 declare module '*.yaml' {
-  const data: any;
+  interface MyYamlData {
+    SITE: {
+      name: string;
+      website: string;
+      trailingSlash: boolean;
+      title?: string;
+      description?: string;
+    };
+    METADATA: {
+      description: string;
+      title?: string;
+    };
+    APP_BLOG?: {
+      isEnabled?: boolean;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      list?: any; // Tambahkan komentar ini jika Anda tidak ingin mempersempit tipe 'list'
+    };
+    site?: {
+      name?: string;
+      description?: string;
+    };
+    contact?: {
+      email?: string;
+    };
+  }
+  const data: MyYamlData;
   export default data;
 }
