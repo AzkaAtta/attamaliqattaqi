322468e2203988576d8d9738da265cc7ceae5c25
auradigital.id <143815833+AzkaAtta@users.noreply.github.com>
2025-08-03 22:31:50 +0700

    Update images.ts

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,3 +1,47 @@
+import { isUnpicCompatible, unpicOptimizer, astroAssetsOptimizer } from './images-optimization';
+import type { ImageMetadata } from 'astro';
+import type { OpenGraph } from '@astrolib/seo';
+
+const load = async function () {
+  let images: Record<string, () => Promise<unknown>> | undefined = undefined;
+  try {
+    images = import.meta.glob('~/assets/images/**/*.{jpeg,jpg,png,tiff,webp,gif,svg,JPEG,JPG,PNG,TIFF,WEBP,GIF,SVG}');
+  } catch (error) {
+    // continue regardless of error
+  }
+  return images;
+};
+
+let _images: Record<string, () => Promise<unknown>> | undefined = undefined;
+
+export const fetchLocalImages = async () => {
+  _images = _images || (await load());
+  return _images;
+};
+
+export const findImage = async (
+  imagePath?: string | ImageMetadata | null
+): Promise<string | ImageMetadata | undefined | null> => {
+  if (typeof imagePath !== 'string') {
+    return imagePath;
+  }
+
+  if (imagePath.startsWith('http://') || imagePath.startsWith('https://') || imagePath.startsWith('/')) {
+    return imagePath;
+  }
+
+  if (!imagePath.startsWith('~/assets/images')) {
+    return imagePath;
+  }
+
+  const images = await fetchLocalImages();
+  const key = imagePath.replace('~/', '/src/');
+
+  return images && typeof images[key] === 'function'
+    ? ((await images[key]()) as { default: ImageMetadata })['default']
+    : null;
+};
+
 export const adaptOpenGraphImages = async (
   openGraph: OpenGraph = {},
   astroSite: URL | undefined = new URL('')
@@ -20,7 +64,7 @@ export const adaptOpenGraphImages = async (
           };
         }
 
-        let _image: any; // ✅ hanya ini yang perlu kamu ubah
+        let _image: any;
 
         if (
           typeof resolvedImage === 'string' &&
