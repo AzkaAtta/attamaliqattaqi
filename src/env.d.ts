690555aefd5185a5f578593114035a19deee14df
auradigital.id <143815833+AzkaAtta@users.noreply.github.com>
2025-08-03 08:52:42 +0700

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-// Versi dari local Anda (HEAD)
-/// <reference types="astro/client" />
-/// <reference types="vite/client" />
-interface MyYamlData {
-  SITE: {
-    name: string;
-  };
-}
-=======
-// Versi dari remote (origin/main)
 /// <reference types="astro/client" />
 /// <reference types="vite/client" />
 /// <reference types="../vendor/integration/types" />
+
 declare module '*.yaml' {
   interface MyYamlData {
     SITE: {
@@ -20,5 +10,7 @@ declare module '*.yaml' {
       trailingSlash: boolean;
     };
   }
+
+  const data: MyYamlData;
+  export default data;
 }
->>>>>>> <commit_hash_remote>
