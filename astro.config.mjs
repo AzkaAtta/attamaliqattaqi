2ce9e7d50da2d0789bfd4937202086ab1ce84dd5
auradigital.id <143815833+AzkaAtta@users.noreply.github.com>
2025-08-06 17:31:43 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import path from 'path';
 
 export default defineConfig({
-  site: 'https://auradigital.id', // Ganti dengan domain kamu
+  site: 'https://auradigital.id',
   integrations: [sitemap()],
+  vite: {
+    resolve: {
+      alias: {
+        'astrowind:config': path.resolve('./src/config.ts'), // atau file config kamu
+      },
+    },
+  },
 });
