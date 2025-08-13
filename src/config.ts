e74453ab851e99fb940005e7d0e53f7bf36ff4d5
auradigital.id <143815833+AzkaAtta@users.noreply.github.com>
2025-08-13 11:07:41 +0700

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+// src/config.ts
+export const SITE = {
+  title: "Jasa Buzzer Sosial Media Murah, Aman dan Terpercaya
+",
+  description: "Support Semua Platform Sosial Media – Satu Layanan, Banyak Hasil, Satu Klik, Ribuan Interaksi – Tanpa Ribet, Tanpa Repot",
+  url: "https://auradigital.id",
+  defaultImage: "/images/social-card.png",
+};
